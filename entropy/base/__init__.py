a05468c28f7dbf7b40473e1e6e2ff5f14b502cfa
Y3K <dboy.y3k@gmail.com>
2014-11-13 00:10:06 -0600

    Clean up, update & Flake8

--- entropy/base/__init__.py
+++ entropy/base/__init__.py
@@ -1,21 +1,23 @@
 import datetime
-import functools
 
-
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes import generic
-from django.contrib.contenttypes.generic import GenericRelation
-from django.contrib.contenttypes.models import ContentType
+# Comment out this because ImagesMixin has been moved to commoncode/images
+# from django.contrib.contenttypes.generic import GenericRelation
+# from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.template.defaultfilters import slugify
+from django.utils.text import Truncator, slugify
 
-
-from ..fields import *
+from ..fields import (
+    EnabledField,
+    ShortTitleField,
+    SlugField,
+    TitleField
+)
 from .. import settings as entropy_settings
-from django.conf import settings
 
 
 # Generic
-
 class GenericMixin(models.Model):
     '''Generic Foreign Key fields'''
     content_type = models.ForeignKey('contenttypes.ContentType')
@@ -29,66 +31,61 @@ class GenericMixin(models.Model):
 
 
 # Text & Content Mixins
-
 class NameMixin(models.Model):
     '''
     Name mixin
 
     Should not be used with TitleMixin
+
     '''
 
-    name = models.CharField(
-        max_length=1024)
-
-    name_plural = models.CharField(
-        blank=True,
-        max_length=1024)
+    name = models.CharField(max_length=1024)
+    name_plural = models.CharField(max_length=1024, blank=True)
 
     class Meta:
         abstract = True
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
 class TextMixin(models.Model):
-
-    text = models.TextField(
-        blank=True,
-        default='')
+    text = models.TextField(blank=True, default='')
 
     class Meta:
         abstract = True
 
     def __init__(self, *args, **kwargs):
-        if not hasattr(self, '__unicode__'):
-            def _unicode(self):
+        if not hasattr(self, '__str__'):
+            def _str(self):
                 return self.text
-            setattr(self.__class__, '__unicode__', _unicode)
+
+            setattr(self.__class__, '__str__', _str)
+
         super(TextMixin, self).__init__(*args, **kwargs)
 
     def truncated(self, words=25):
-        return truncate(self.text, words)
+        return Truncator(self.text).words(words, truncate=' ...')
 
     def truncated_chars(self, characters=50):
-        return truncate_chars(self.text, characters)
+        return Truncator(self.text).chars(characters)
 
 
 class TitleMixin(models.Model):
     '''
     Title mixin.
 
-    Should not be used w/ NameMixin
+    Should not be used with NameMixin
+
     '''
 
     title = TitleField()
-
     short_title = ShortTitleField()
 
     class Meta:
         abstract = True
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 
@@ -102,12 +99,11 @@ class BaseSlugMixin(object):
 
     '''
 
-
     def save(self, *args, **kwargs):
         if not self.id and not self.slug:
-
             if hasattr(self, 'title'):
                 sluggable = getattr(self, 'title', None)
+
             if hasattr(self, 'name'):
                 sluggable = getattr(self, 'name', None)
 
@@ -119,18 +115,18 @@ class BaseSlugMixin(object):
         super(BaseSlugMixin, self).save(*args, **kwargs)
 
     def slugify_uniquely(self, sluggable):
-
         model = self.__class__
         slug = slug_prefix = slugify(sluggable)
         index = 0
+
         while model.objects.filter(slug=slug).exists():
             index += 1
             slug = slug_prefix + '-' + str(index)
+
         return slug
 
 
 class SlugMixin(BaseSlugMixin, models.Model):
-
     slug = SlugField()
 
     class Meta:
@@ -138,7 +134,6 @@ class SlugMixin(BaseSlugMixin, models.Model):
 
 
 class SlugUniqueMixin(BaseSlugMixin, models.Model):
-
     slug = SlugField(unique=True)
 
     class Meta:
@@ -146,12 +141,14 @@ class SlugUniqueMixin(BaseSlugMixin, models.Model):
 
 
 # Meta & Status Mixins
-
-
 class MetadataMixin(models.Model):
     '''
-    HTML Metadata Mixin - for all those groovy HTML meta tags that can be important for SEO
+    HTML Metadata Mixin
+
+    For all those groovy HTML meta tags that can be important for SEO
+
     '''
+
     meta_title = models.CharField(max_length=255, blank=True, null=True)
     meta_description = models.CharField(max_length=255, blank=True, null=True)
     meta_keywords = models.CharField(max_length=255, blank=True, null=True)
@@ -161,13 +158,13 @@ class MetadataMixin(models.Model):
 
 
 class CreatedMixin(models.Model):
-
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
-        getattr(settings, 'AUTH_USER_MODEL', 'auth.User'),      # adds support for custom user models in 1.5
+        get_user_model(),
         blank=True,
-        null=True, # For now.
-        related_name='%(app_label)s_%(class)s_created_by')
+        null=True,
+        related_name='%(app_label)s_%(class)s_created_by'
+    )
 
     class Meta:
         abstract = True
@@ -175,18 +172,21 @@ class CreatedMixin(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.created_at = datetime.datetime.now()
-            # self.created_by = request.user # Should this be assigned in admin? (as it depends on 'request')
+
+            # Should this be assigned in admin? (as it depends on 'request')
+            # self.created_by = request.user
+
         super(CreatedMixin, self).save(*args, **kwargs)
 
 
 class ModifiedMixin(models.Model):
-
     modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(
-        getattr(settings, 'AUTH_USER_MODEL', 'auth.User'),      # adds support for custom user models in 1.5
+        get_user_model(),
         blank=True,
-        null=True, # For now.
-        related_name='%(app_label)s_%(class)s_modified_by')   # XXX Should use a template
+        null=True,
+        related_name='%(app_label)s_%(class)s_modified_by'
+    )
 
     class Meta:
         abstract = True
@@ -194,27 +194,30 @@ class ModifiedMixin(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.modified_at = datetime.datetime.now()
-            # self.modified_by = request.user # Should this be assigned in admin?
+
+            # Should this be assigned in admin?
+            # self.modified_by = request.user
+
         super(ModifiedMixin, self).save(*args, **kwargs)
 
 
 class OwnerMixin(models.Model):
-
     owned_by = models.ForeignKey(
-        getattr(settings, 'AUTH_USER_MODEL', 'auth.User'),      # adds support for custom user models in 1.5
+        get_user_model(),
         blank=True,
-        null=True, # For now.
-        related_name='%(app_label)s_%(class)s_owned_by')
+        null=True,
+        related_name='%(app_label)s_%(class)s_owned_by'
+    )
 
     class Meta:
         abstract = True
 
 
 # Start & End
-
 class StartEndManager(models.Manager):
     def current(self):
         '''Return only models whose start/end bound now'''
+
         now = datetime.datetime.now()
         return self.get_query_set().filter(
             start__lte=now,
@@ -223,41 +226,34 @@ class StartEndManager(models.Manager):
 
 
 class StartEndBaseMixin(models.Model):
-
     class Meta:
         abstract = True
 
-    # def save(self, *args, **kwargs):
-    #     '''
-    #     Validate start and end fields
-    #     '''
-    #     if self.end:
-    #         if self.start > self.end:
-    #             pass
-    #             # make this work or move it to a Form?
-    #             # raise ValidationError("'end' should not be before 'start'")
-    #     super(StartEndBaseMixin, self).save(*args, **kwargs)
+    """
+    def save(self, *args, **kwargs):
+        '''
+        Validate start and end fields
+        '''
+        if self.end:
+            if self.start > self.end:
+                pass
+                # make this work or move it to a Form?
+                # raise ValidationError("'end' should not be before 'start'")
+        super(StartEndBaseMixin, self).save(*args, **kwargs)
+    """
 
 
 class StartEndMixin(StartEndBaseMixin):
-
     start = models.DateTimeField()
-    end = models.DateTimeField(
-        blank=True,
-        null=True)
+    end = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         abstract = True
 
 
 class StartEndBetaMixin(StartEndBaseMixin):
-
-    start = models.DateTimeField(
-        blank=True,
-        null=True)
-    end = models.DateTimeField(
-        blank=True,
-        null=True)
+    start = models.DateTimeField(blank=True, null=True)
+    end = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         abstract = True
@@ -266,15 +262,16 @@ class StartEndBetaMixin(StartEndBaseMixin):
 class EnabledManager(models.Manager):
     def enabled(self):
         '''Return only models which are enabled'''
+
         return self.get_query_set().filter(enabled=True)
 
     def disabled(self):
         '''Return only models which are disabled'''
+
         return self.get_query_set().filter(enabled=False)
 
 
 class EnabledMixin(models.Model):
-
     enabled = EnabledField()
 
     # @@@ TODO add the enabled pass through manager
@@ -286,13 +283,12 @@ class EnabledMixin(models.Model):
 class PublishingManager(models.Manager):
     def published(self):
         '''Return only models which are enabled'''
+
         return self.get_query_set().filter().enabled().current()
 
 
 class PublishingMixin(StartEndMixin, EnabledMixin):
-    '''
-    Published Mixin, depends on EnabledMixin, StartEndMixin
-    '''
+    '''Published Mixin, depends on EnabledMixin, StartEndMixin'''
 
     publish = EnabledField()
 
@@ -301,41 +297,43 @@ class PublishingMixin(StartEndMixin, EnabledMixin):
 
 
 # Functional Mixins
-
 class OrderingMixin(models.Model):
-
-    order = models.PositiveIntegerField(
-        blank=True,
-        default=0)
+    order = models.PositiveIntegerField(blank=True, default=0, index_db=True)
 
     class Meta:
         abstract = True
-        ordering = ('order',)
+        ordering = ('order', )
 
 
 # Hofstadter
-
-RATING_CHOICES = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
-
 class PriorityMixin(models.Model):
+    RATING_CHOICES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5)
+    )
 
     severity = models.PositiveIntegerField(
         blank=True,
         choices=RATING_CHOICES,
-        null=True)
+        null=True
+    )
 
     priority = models.PositiveIntegerField(
         blank=True,
         choices=RATING_CHOICES,
-        null=True)
+        null=True
+    )
 
     class Meta:
         abstract = True
 
 
+# FIXME this will probably be now commoncode/images
 # Images
-
-
+"""
 class ImageMixin(models.Model):
     '''
     Super neat ImageMixin model.
@@ -344,9 +342,9 @@ class ImageMixin(models.Model):
     to via gfk.
 
     The images will be available via:
-
         parent_obj.image -- the first image object, no queries
-        parent_obj.images -- the queryset -- one query which is saved as self._queryset for future use
+        parent_obj.images -- the queryset -- one query which is saved as
+            self._queryset for future use
 
 
     In templates with Sorl
@@ -362,7 +360,9 @@ class ImageMixin(models.Model):
         {% endthumbnail %}
     {% endfor %}
     {% endwith %}
+
     '''
+
     image_set = GenericRelation('entropy.Image')
 
     class Meta:
@@ -393,12 +393,11 @@ class ImageMixin(models.Model):
             object_id=self.id,
             is_icon=True
         ))
+"""
+
 
 class BaseLinkMixin(models.Model):
-
-    content_object = generic.GenericForeignKey(
-        'content_type',
-        'object_id')
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     class Meta:
         abstract = True
@@ -408,21 +407,25 @@ class LinkURLMixin(BaseLinkMixin):
     '''
     Class for generating get_absolute_url from either the
     linked object, or the overriding url field
+
     '''
 
     content_type = models.ForeignKey(
         'contenttypes.ContentType',
         blank=True,
-        limit_choices_to={'model__in': entropy_settings.LINKABLE_MODELS },
-        null=True)
+        limit_choices_to={'model__in': entropy_settings.LINKABLE_MODELS},
+        null=True
+    )
     object_id = models.PositiveIntegerField(
         blank=True,
-        null=True)
+        null=True
+    )
 
     url = models.CharField(
         blank=True,
-        help_text="Optionally, override and link to an arbitrary URL",
-        max_length=1024)
+        help_text='Optionally, override and link to an arbitrary URL',
+        max_length=1024
+    )
 
     class Meta:
         abstract = True
@@ -439,20 +442,19 @@ class LinkURLMixin(BaseLinkMixin):
     def clean(self):
         if self.content_object and self == self.content_object:
             from django.core.exceptions import ValidationError
-            raise ValidationError("An object should not link to itself.")
+            raise ValidationError('An object should not link to itself.')
 
 
 class AttributeMixin(models.Model):
     '''Mixin to give dict access to generic Attributes'''
+
     attributes = generic.GenericRelation('Attribute')
 
     class Meta:
         abstract = True
 
     def _attributes(self):
-        return dict(
-            self.attributes.values_list('name', 'value')
-        )
+        return dict(self.attributes.values_list('name', 'value'))
 
     def __getitem__(self, key):
         try:
